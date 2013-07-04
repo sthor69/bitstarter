4473ebeca397dd2f04484b9fe092eca890b50514
Sergio Torassa <example@hotmail.com>
2013-07-04 14:14:20 +0000

    -Third for index.html

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
